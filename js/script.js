9217daa628bbeb7e94f41516926f401997cd4b0b
ezeqquiel <ricarditoricarda@gmail.com>
2026-05-06 11:07:39 -0300

    lo ultimio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@
     hamburger.addEventListener("click", () => {
       navLinks.classList.toggle("open");
     });
+
+
+    const items = document.querySelectorAll('.servicio-item');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+      }, { threshold: 0.2 }); // se activa cuando el 20% del elemento es visible
+
+items.forEach(item => observer.observe(item));
