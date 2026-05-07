1ba9183c28ef09ceb2aa7627d9f60067d5367d7c
ezeqquiel <ricarditoricarda@gmail.com>
2026-05-07 12:14:32 -0300

    retoques3

--- js/script.js
+++ js/script.js
@@ -17,3 +17,18 @@
       }, { threshold: 0.2 }); // se activa cuando el 20% del elemento es visible
 
 items.forEach(item => observer.observe(item));
+
+
+const cards = document.querySelectorAll('.about-card');
+
+const observerCards = new IntersectionObserver((entries) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('visible');
+      }, index * 150); // ← cada tarjeta aparece 150ms después de la anterior
+    }
+  });
+}, { threshold: 0.2 });
+
+cards.forEach(card => observerCards.observe(card));
